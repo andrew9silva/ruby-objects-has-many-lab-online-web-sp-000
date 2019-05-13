240c2d8e9f5033ea42b6c5fb5102c2c1c7731036
Andrew Silva <andrewsilva9@gmail.com>
2019-05-13 03:12:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,12 @@ class Artist
     @@song_count += 1
   end
 
+  def add_song_by_name(song)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+
   def songs
     @songs
   end
